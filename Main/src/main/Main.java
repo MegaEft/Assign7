c28cc72f309f701ded3c35ae204212d2a4e9f9c1
MegaEft <MegaEftStudio@gmail.com>
2013-03-07 23:57:34 -0600

    Update Main.java

--- Main/src/main/Main.java
+++ Main/src/main/Main.java
@@ -26,7 +26,7 @@ public class Main extends Application {
             @Override
             public void handle(ActionEvent event) {
                 System.out.println("Hello World!");
-                //test.
+                //not test.
             }
         });
         
