ad8eb070ffe1e2eca32fab28ddbdf38e609369e9
Daniel Bichel <dan.bichel@gmail.com>
2013-03-07 22:16:30 -0600

    Added Dan Printline

--- Main/src/main/Main.java
+++ Main/src/main/Main.java
@@ -26,7 +26,7 @@ public class Main extends Application {
             @Override
             public void handle(ActionEvent event) {
                 System.out.println("Hello World!");
-                System.out.println("Dan")
+                System.out.println("Dan");
             }
         });
         
