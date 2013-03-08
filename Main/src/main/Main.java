afc83def4f9c016354416cff936077a2dd28ace6
Tyler Schreiner <rkfan500@gmail.com>
2013-03-07 22:37:10 -0600

    test2

--- Main/src/main/Main.java
+++ Main/src/main/Main.java
@@ -25,7 +25,9 @@ public class Main extends Application {
         btn.setOnAction(new EventHandler<ActionEvent>() {
             @Override
             public void handle(ActionEvent event) {
+                System.out.println("fuck this shit");
                 System.out.println("Hello World!");
+                System.out.println("Why can't there be nice client server version control?");
             }
         });
         
