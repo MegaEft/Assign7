b661ef3555a7c1be45b395ac30f1c38072f88b30
Daniel Bichel <dan.bichel@gmail.com>
2013-03-07 23:43:25 -0600

    test1

--- Main/src/main/Main.java
+++ Main/src/main/Main.java
@@ -25,11 +25,7 @@ public class Main extends Application {
         btn.setOnAction(new EventHandler<ActionEvent>() {
             @Override
             public void handle(ActionEvent event) {
-                System.out.println("fuck this shit");
                 System.out.println("Hello World!");
-                System.out.println("Test1");
-                System.out.println("Test2");
-                //CHANGES!!!!!!!!!!
             }
         });
         
