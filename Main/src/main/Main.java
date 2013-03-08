84051b4ba69c2f3cfbd1780dbcc1d6f0787cf187
Hackstrong <Hackstrong@Lappy>
2013-03-08 13:15:33 -0600

    Added Hello World 2

--- Main/src/main/Main.java
+++ Main/src/main/Main.java
@@ -26,6 +26,7 @@ public class Main extends Application {
             @Override
             public void handle(ActionEvent event) {
                 System.out.println("Hello World!");
+                System.out.println("Print line 1");
                 //yes test.
                 //not test.
             }
