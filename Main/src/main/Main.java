c8b9d771d5faf33b55e648a35a90c1d0c12ec00c
Daniel Bichel <dan.bichel@gmail.com>
2013-03-07 23:50:44 -0600

    test1

--- Main/src/main/Main.java
+++ Main/src/main/Main.java
@@ -26,6 +26,7 @@ public class Main extends Application {
             @Override
             public void handle(ActionEvent event) {
                 System.out.println("Hello World!");
+                //test.
             }
         });
         
